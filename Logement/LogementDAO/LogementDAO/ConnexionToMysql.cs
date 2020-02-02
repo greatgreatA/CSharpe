0cb43adef314b7c94862d94ddac5c62ce668a7c7
greatgreatA <gilles.greta.mte94@gmail.com>
2020-02-02 09:33:29 +0100

    première synchronisation

---
+++ Logement/LogementDAO/LogementDAO/ConnexionToMysql.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace LogementDAO
+{
+    public class ConnexionToMysql
+    {
+        private String connexionString;//"SERVER=" ";" "DATABASE"= ";" "UID=" ";" "PASSWORD=" ";"
+        private MySqlConnection mysqlConnexion;
+        private static readonly object mylock = new object();
+        private static ConnexionToMysql uneInstance = null;
+
+        //creation de la connexion
+        private ConnexionToMysql(String connectionSting)
+        {
+            try
+            {
+                mysqlConnexion = new MySqlConnection(connectionSting);
+            }
+            catch (MySqlException me)
+            {
+                throw me;
+            }
+        }
+        //instanciation de la classe
+        public static ConnexionToMysql getInstance(string stringDeConnexion)
+        {
+            lock((mylock))
+            {
+                try
+                {
+                    if (uneInstance != null)
+                    {
+                        return uneInstance;
+                    }
+                    else
+                    {
+                        uneInstance = new ConnexionToMysql(stringDeConnexion);
+                        return uneInstance;
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            
+        }
+        //ouverture de la connexion
+        public void openConnexion()
+        {
+            try
+            {
+                mysqlConnexion.Open();
+            }
+            catch (MySqlException e)
+            {
+
+                throw e;
+            }
+        }
+        //fermeture de la connexion
+        public void closeConnexion()
+        {
+            try
+            {
+                mysqlConnexion.Close();
+            }
+            catch (MySqlException e)
+            {
+                throw e;
+            }
+        }
+        
+        public MySqlCommand prepareCommande(string requete)
+        {
+            try
+            {
+                MySqlCommand c = new MySqlCommand(requete, mysqlConnexion);
+                return c;
+            }
+            catch (MySqlException e)
+            {
+
+                throw e;
+            }
+        }
+    }
+
+
+}
---
+++ Logement/ModeleLog/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ModeleLog
+{
+    public class Class1
+    {
+    }
+}
